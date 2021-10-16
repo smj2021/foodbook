9936d659cb5772bc2e5cd5a21085cef542f13516
smj2021 <mjschwartz515@gmail.com>
2021-10-16 14:25:20 -0400

    add profile routes and ejs docs

--- routes/profiles.js
+++ routes/profiles.js
@@ -5,7 +5,10 @@ import * as profilesCtrl from '../controllers/profiles.js'
 const router = Router();
 
 //Routes to landing page
-router.get('/', isLoggedIn, profilesCtrl.index)
+router.get('/', isLoggedIn, profilesCtrl.index);
+router.get('/:id', isLoggedIn, profilesCtrl.show);
+router.get('/:id/edit', isLoggedIn, profilesCtrl.edit);
+router.put('/:id/', isLoggedIn, profilesCtrl.update);
 
 
 function isLoggedIn(req, res, next) {

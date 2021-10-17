1c887f2fb6c868f86412902ef4b3efa63bdb22cd
smj2021 <mjschwartz515@gmail.com>
2021-10-17 14:47:27 -0400

    add recipe edit controller

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -59,6 +59,20 @@ function addNote(req, res) {
     })
 }
 
+function edit(req, res){
+    Recipe.findById(req.params.id)
+    .then(function(recipe){
+        res.render('recipes/edit', {
+            title: `Editing ${recipe.name}`,
+            recipe
+        })
+    })
+    .catch(function(err){
+        console.log(err)
+        res.redirect('/')
+    })
+}
+
 // function deleteRecipe (req, res) {
 //     Recipe.findById(req.params.id, {
 

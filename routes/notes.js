2bb53b033891186beb32661cd21bea8a517d0841
smj2021 <mjschwartz515@gmail.com>
2021-10-18 11:51:48 -0400

    add note delete router

--- routes/notes.js
+++ routes/notes.js
@@ -7,6 +7,8 @@ router.get('/new', isLoggedIn, notesCtrl.new);
 router.post('/', isLoggedIn, notesCtrl.create);
 router.get('/:id/edit', isLoggedIn, notesCtrl.edit);
 router.put('/:id/', isLoggedIn, notesCtrl.update);
+router.delete('/:id', isLoggedIn, notesCtrl.delete);
+
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
